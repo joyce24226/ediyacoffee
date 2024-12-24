01fafedeffb80bbbeac963b2acff330bc3c388be
joyce24226 <leehwayoung926@gmail.com>
2024-12-24 10:43:02 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -15,47 +15,51 @@ $(document).ready(function () {
     },
     loop: true,
   });
+// 탭 초기 설정
+$(".tab-cont .cont").hide(); // 모든 탭 내용 숨김
+$(".tab-btn li").eq(0).addClass("active"); // 첫 번째 탭 활성화
+$(".tab-cont .cont").eq(0).show(); // 첫 번째 탭 내용 표시
 
-  $(".tab-cont .cont").hide();
+// Swiper 인스턴스 저장용 변수
+let swipers = {};
 
-  $(".tab-btn li").eq(0).addClass("active");
-  $(".tab-cont .cont").eq(0).show();
-
-  $(".tab-btn li").click(function () {
+// 탭 전환 기능
+$(".tab-btn li").click(function () {
     $(this).addClass("active").siblings().removeClass("active");
 
     var indexNum = $(this).index();
     $(".tab-cont .cont").eq(indexNum).show().siblings().hide();
-  })
 
-  var tabInside = new Swiper(".sw1 .tab-inside", {
-    slidesPerView: 2.5,
-    spaceBetween: 40,
-    // centeredSlides: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    loop: true,
-  });
-  var tabInside = new Swiper(".sw2 .tab-inside", {
-    slidesPerView: 2.5,
-    spaceBetween: 40,
-    // centeredSlides: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    loop: true,
-  });
-  var tabInside = new Swiper(".sw3 .tab-inside", {
-    slidesPerView: 2.5,
-    spaceBetween: 40,
-    // centeredSlides: true,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    loop: true,
-  });
+    // Swiper 재초기화
+    initializeSwiper(indexNum);
+});
+
+// Swiper 초기화 함수
+function initializeSwiper(index) {
+    const tabClasses = ["sw1", "sw2", "sw3"]; // 탭 클래스 배열
+    const targetClass = `.${tabClasses[index]} .tab-inside`;
+
+    // Swiper가 이미 초기화된 경우 삭제
+    if (swipers[index]) {
+        swipers[index].destroy(true, true);
+    }
+
+    // 새로운 Swiper 생성
+    swipers[index] = new Swiper(targetClass, {
+        slidesPerView: 2.5,
+        spaceBetween: 40,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        loop: true,
+    });
+}
+
+// 처음 Swiper 초기화
+initializeSwiper(0);
+
+
+
+
 })
